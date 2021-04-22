f3c0a07d4bdf6d2b76b6ffd4e8f81ba4e73d8848
YamkelaMathontsi <yamkelapj@gmail.com>
2021-04-22 09:57:24 +0200

    added new file

---
+++ exercise.py
@@ -0,0 +1,8 @@
+AveSpeed = float(input("What was your speed"))
+if AveSpeed < 60:
+    print("OK")
+else:
+    print("Points 2")
+DemeritPoints = float(input("Demerit Points"))
+if DemeritPoints < 12:
+    print("Time to go to Jail")
